4a2203472c82d7b6552ccddc1dab74819cddaa50
Nikolai Brendler <nbrendler@gmail.com>
2019-10-17 22:57:12 +0800

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,86 @@
 use std::process::{Command, Stdio};
 
-use serde::{Deserialize, Serialize};
+use serde;
+use serde::Deserialize;
 use serde_json;
+use serde_json::{from_slice, from_value, json, to_writer, Value};
 
 #[derive(Debug, Deserialize)]
 pub struct Channel {
     pub name: String,
-    pub members_type: String,
     pub topic_type: String,
 }
 
-// TODO: date fields
 #[derive(Debug, Deserialize)]
 pub struct Conversation {
     pub id: String,
     pub channel: Channel,
     pub unread: bool,
-    pub member_status: String,
 }
 
-#[derive(Deserialize)]
-struct ConversationResultInner {
-    conversations: Vec<Conversation>,
+#[derive(Debug, Deserialize)]
+pub struct MessageBody {
+    pub body: String,
 }
 
-#[derive(Deserialize)]
-struct ConversationResult {
-    result: ConversationResultInner,
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+#[allow(non_camel_case_types)]
+pub enum MessageType {
+    text { text: MessageBody },
+    attachment {},
+    unfurl {},
 }
 
-#[derive(Serialize)]
-struct KBCommand {
-    pub method: &'static str,
+#[derive(Debug, Deserialize)]
+pub struct MessageWrapper {
+    pub msg: Message,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Message {
+    pub channel: Channel,
+    pub content: MessageType,
+    pub sender: Sender,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Sender {
+    pub username: String,
+    pub device_name: String,
 }
 
 pub fn list_conversations() -> Vec<Conversation> {
-    let result = keybase_exec(KBCommand { method: "list" }).unwrap();
-    let parsed_result: ConversationResult =
-        serde_json::from_slice(result.as_slice()).expect("Failed to parse conversation result");
-    parsed_result.result.conversations
+    let result = keybase_exec(json!({
+        "method": "list"
+    }))
+    .unwrap();
+
+    let mut parsed: Value = from_slice(result.as_slice()).unwrap();
+    from_value(parsed["result"]["conversations"].take())
+        .expect("Failed to deserialize conversation list")
 }
 
-fn keybase_exec(command: KBCommand) -> serde_json::Result<Vec<u8>> {
+pub fn read_conversation<T: Into<String>>(name: T, count: u32) -> Vec<Message> {
+    let result = keybase_exec(serde_json::json!({
+        "method": "read",
+        "params": {
+            "options": {
+                "channel": {"name": name.into()},
+                "pagination": {"num": count}
+            }
+        }
+    }))
+    .unwrap();
+    let mut parsed: Value = from_slice(result.as_slice()).unwrap();
+    from_value::<Vec<MessageWrapper>>(parsed["result"]["messages"].take())
+        .expect("Failed to deserialize messages")
+        .into_iter()
+        .map(|wrapper| wrapper.msg)
+        .collect()
+}
+
+fn keybase_exec(command: Value) -> serde_json::Result<Vec<u8>> {
     let mut child = Command::new("keybase")
         .arg("chat")
         .arg("api")
@@ -52,7 +91,7 @@ fn keybase_exec(command: KBCommand) -> serde_json::Result<Vec<u8>> {
 
     {
         let stdin = child.stdin.as_mut().expect("Failed to get child stdin");
-        serde_json::to_writer(stdin, &command)?;
+        to_writer(stdin, &command)?;
     }
 
     let output = child.wait_with_output().expect("No Keybase output");
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-use client::{list_conversations, Conversation};
+use client::{list_conversations, read_conversation, MessageType};
 
 fn main() {
-    let convos: Vec<Conversation> = list_conversations();
-    let channel_names: Vec<String> = convos.into_iter().map(|c| c.channel.name).collect();
-    println!("{:?}", channel_names);
+    let convos = list_conversations();
+    println!("{:?}", convos);
+
+    let messages = read_conversation(&convos[0].channel.name, 20);
+    for m in messages.iter().rev() {
+        match &m.content {
+            MessageType::text { text } => {
+                println!("{}: {}", m.sender.username, text.body);
+            }
+            _ => {}
+        }
+    }
 }
